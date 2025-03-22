b56350bebda22acbfd93242a11f7b0a1ddd983d3
Stranger <ethicalfighter56@gmail.com>
2025-03-23 04:42:48 +0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,12 +57,12 @@ TW_DEFAULT_BRIGHTNESS := 162
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_INCLUDE_NTFS_3G := true
-LZMA_RAMDISK_TARGETS := recovery
-TW_USE_NEW_MINADBD := true
-TW_EXCLUDE_PYTHON := true
 
 # exFAT drivers included in the kernel
 TW_NO_EXFAT_FUSE := true
+
+# Encryption support
+TW_INCLUDE_CRYPTO := true
 
 # Asian region languages
 TW_EXTRA_LANGUAGES := true
